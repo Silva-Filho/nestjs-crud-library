42c2bde91f10c3bbfd529099ef22a8683a5d2675
Silva Filho <tarusilva@gmail.com>
2022-08-18 11:05:24 -0300

    Adição de pipes ao app para validar os dados.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    // Pipes
+    app.useGlobalPipes(
+        new ValidationPipe({
+            // transforma o tipo do dado de entrada no tipo esperado.
+            transform: true,
+            // Não passa adiante informação a mais.
+            whitelist: true,
+            // Gera erro se houver dado de entrada a mais.
+            forbidNonWhitelisted: true,
+        }),
+    );
+
+    await app.listen(8000);
 }
 bootstrap();
