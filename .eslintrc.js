8af994d40b6987c53690467945764597f2d7605f
Silva Filho <tarusilva@gmail.com>
2022-08-31 18:04:30 -0300

    Atualização das regras do ESLint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,55 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "tsconfig.json",
-    tsconfigRootDir : __dirname, 
-    sourceType: "module",
-  },
-  plugins: [
-    "@typescript-eslint/eslint-plugin", 
-    /* "prettier" */
-],
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    /* "plugin:prettier/recommended", */
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: [".eslintrc.js"],
-  rules: {
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "import/prefer-default-export": "off",
-    "indent": [
-        "error",
-        4
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: "tsconfig.json",
+        tsconfigRootDir : __dirname, 
+        sourceType: "module",
+    },
+    plugins: [
+        "@typescript-eslint/eslint-plugin", 
+        /* "prettier" */
     ],
-    "linebreak-style": [
-        "error",
-        "unix"
+    extends: [
+        "plugin:@typescript-eslint/recommended",
+        /* "plugin:prettier/recommended", */
     ],
-    "quotes": [
-        "error",
-        "double"
-    ],
-    "semi": [
-        "error",
-        "always"
-    ],
-    /* "prettier/prettier": "error" */
-  },
+    root: true,
+    env: {
+        node: true,
+        jest: true,
+    },
+    ignorePatterns: [".eslintrc.js"],
+    rules: {
+        "@typescript-eslint/interface-name-prefix": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "import/prefer-default-export": "off",
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "array-bracket-spacing": [
+            "error", 
+            "always"
+        ],
+        "space-in-parens": [
+            "error", 
+            "always"
+        ],
+        /* "prettier/prettier": "error" */
+    },
 };
