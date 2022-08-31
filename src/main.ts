a9ed8b51c3cee20a99eb185db6196b533c810d62
Silva Filho <tarusilva@gmail.com>
2022-08-31 18:03:37 -0300

    Adição do logger no arquivo main.

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,24 @@
-import { ValidationPipe } from "@nestjs/common";
+import { Logger, ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create( AppModule );
+    const logger = new Logger();
 
     // Pipes
     app.useGlobalPipes(
-        new ValidationPipe({
+        new ValidationPipe( {
             // transforma o tipo do dado de entrada no tipo esperado.
             transform: true,
             // Não passa adiante informação a mais.
             whitelist: true,
             // Gera erro se houver dado de entrada a mais.
             forbidNonWhitelisted: true,
-        }),
+        } ),
     );
-
-    await app.listen(8000);
+    
+    await app.listen( 8080 );
+    logger.log( `O servidor está rodando na porta ${ await app.getUrl() }.` );
 }
 bootstrap();
