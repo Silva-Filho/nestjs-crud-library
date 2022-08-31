070af1d0016a7799ab7511c40ad7b7b70679afb5
Silva Filho <tarusilva@gmail.com>
2022-08-31 18:11:49 -0300

    Adição dos módulos DatabaseModule, AuthorsModule e BooksModule ao AppModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,38 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+// import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { AuthorModule } from "./author/author.module";
+import { AuthorsModule } from "./authors/authors.module";
+// import { Author } from "./authors/entities/author.entity";
+// import { Book } from "./books/entities/book.entity";
+import { DatabaseModule } from "./database/database.module";
+import { BooksModule } from "./books/books.module";
 
-@Module({
-    imports: [AuthorModule],
-    controllers: [AppController],
-    providers: [AppService],
-})
+@Module( {
+    imports: [
+        ConfigModule.forRoot( { 
+            // envFilePath: [".env"], 
+            isGlobal: true, 
+            // cache: true,
+        } ),
+        /* TypeOrmModule.forRoot( {
+            type: process.env.DB_TYPE as any,
+            host: process.env.DB_HOST,
+            port: parseInt( <string>process.env.DB_PORT ),
+            username: process.env.DB_USER,
+            password: String( process.env.DB_PASSWORD ),
+            database: process.env.DB_DATABASE,
+            entities: [ Author, Book ],
+            synchronize: true,
+            logging: true,
+        } ), */
+        DatabaseModule, 
+        AuthorsModule, 
+        BooksModule,
+    ],
+    controllers: [ AppController ],
+    providers: [ AppService ],
+} )
 export class AppModule {}
