4d78bbab2c34730cb2b0511e55446159f9d32054
Silva Filho <tarusilva@gmil.com>
2022-09-01 15:53:46 -0300

    Criação dos DTOs de cadastro e atualização para Book.

---
+++ src/books/dto/create-book.dto.ts
@@ -0,0 +1,36 @@
+import { 
+    IsDate, 
+    IsInt, 
+    IsNotEmpty, 
+    // IsNumber, 
+    IsOptional, 
+    IsPositive, 
+    IsString, 
+    MaxLength 
+} from "class-validator";
+
+export class CreateBookDto {
+    @IsPositive()
+    @IsInt()
+    @IsNotEmpty()
+        authorId: number;
+    
+    @MaxLength( 200 )
+    @IsString()
+    @IsNotEmpty()
+        name: string;
+
+    @MaxLength( 100 )
+    @IsString()
+    @IsOptional()
+        publisher: string;
+
+    @MaxLength( 50 )
+    @IsString()
+    @IsNotEmpty()
+        genre: string;
+
+    @IsDate()
+    @IsOptional()
+        publishedAt: string;
+}
