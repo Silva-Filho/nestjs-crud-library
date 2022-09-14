05fbaee2fb10ed7a40dfec5f58f3ba1a9465569b
Silva Filho <tarusilva@gmail.com>
2022-09-14 17:07:22 -0300

    feat: cria o DTO de controle de criação de users
    Cria um DTO para validar a informação de criação de um usuario.

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,41 @@
-export class CreateUserDto {}
+import {
+    IsEmail,
+    IsIdentityCard,
+    IsInt,
+    IsNotEmpty,
+    IsOptional,
+    IsPhoneNumber,
+    IsPositive,
+    IsString,
+    Length,
+    MaxLength
+} from "class-validator";
+
+export class CreateUserDto {
+    @MaxLength( 120 )
+    @IsString()
+    @IsNotEmpty()
+        name: string;
+
+    @IsPositive()
+    @IsInt()
+    @IsOptional()
+        age: number;
+
+    @MaxLength( 50 )
+    @IsEmail()
+    @IsNotEmpty()
+        email: string;
+
+    @IsPhoneNumber( "BR" )
+    // @Length( 11, 11 )
+    @IsString()
+    @IsOptional()
+        telephone: string;
+
+    @IsIdentityCard( "pt-BR" )
+    @Length( 11, 11 )
+    @IsString()
+    @IsNotEmpty()
+        cpf: string;
+}
